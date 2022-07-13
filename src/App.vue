23a3aa43076d0bb9a194fbfc30f60c96caf71da0
kerry_chen <kerry_chen@paradise-soft.com.tw>
2022-07-14 00:21:06 +0800

    feat: add two apis

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <script setup>
 import { Ws } from '@/websocket/ws.ts'
-import { onMounted } from 'vue'
 
-let orderBookWs = null
+// OrderBook WebSocket API:
+let orderBookWs = new Ws('wss://ws.btse.com/ws/oss/futures')
 
-onMounted(() => {
-  orderBookWs = new Ws('wss://ws.btse.com/ws/oss/futures')
-})
+const subOrderBook = () => {
+  const reqData = {
+    op: 'subscribe',
+    args: ['update:BTCPFC']
+  }
+  orderBookWs.send(reqData)
+}
+
+// Last price WebSocket API:
+let lastPriceWs = new Ws('wss://ws.btse.com/ws/futures')
+
+const getTradeHistory = () => {
+  const reqData = {
+    op: 'subscribe',
+    args: ['tradeHistoryApi:BTCPFC']
+  }
+  lastPriceWs.send(reqData)
+}
 </script>
 
 <template>
   <div>
     trade work!!!
+
+    <h1 @click="subOrderBook">orderBookWs</h1>
+    <h1 @click="getTradeHistory">lastPriceWs</h1>
   </div>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
